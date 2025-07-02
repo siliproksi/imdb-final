94537222482aaf19e41da22961ba37fa6879b8a6
Muhamed Cicak <silidrone@gmail.com>
2025-07-02 12:54:38 -0600

    changed docker to add nginx and letsencrypt jwilder to deploy to codeise url

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker compose up -d
-echo "Frontend: http://localhost:3000"
-echo "Backend: http://localhost:8000"
+docker network create imdb 2>/dev/null || true
+docker compose up --build
+echo "Frontend: https://imdbfinal.codeise.com"
+echo "Backend: https://imdbfinalb.codeise.com"
